91537f76003ecb4e21b7d3bf4fcfce827c9f3204
killNit <rileysanti@gmail.com>
2020-10-05 16:04:51 -0600

    review_controller test ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   namespace :api do 
+    #This will list ALL review in database, for database validation. 
+    #Remove the line below to disable view of all reviews.
+    get "/reviews/all", to: "reviews#all"
     resources :coffee_shops do
       resources :reviews
     end
