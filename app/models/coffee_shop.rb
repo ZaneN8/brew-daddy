c6dd5a991dd75b624d4e62edeb8ac345854d56b9
Jon Roberts <kewlguy@gmail.com>
2020-10-06 21:22:44 -0600

    added search under model

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -1,4 +1,7 @@
 class CoffeeShop < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
+
+  # This will filter and "search" based on query passed from the search bar. 
+  scope :filter_by_name, -> (name) { where("name ilike ?", "%" + name + "%")}
 end
--- client/src/Components/SearchBar.js
+++ client/src/Components/SearchBar.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form } from "react-bootstrap";
+import { propTypes } from "react-bootstrap/esm/Image";
+
 
 const SearchBar = () => {
+    const [query, setQuery] = useState()
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    propTypes.query(query)
   };
+
 
   return (
     <div>
       <Form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label>Search</Form.Label>
-          <Form.Control autoFocus />
+          <Form.Control autoFocus 
+          placeholder="Enter to search here"
+          name="query"
+          onChange={(e) => setQuery(e.target.value)}/><button type="submit">Search</button>
         </Form.Group>
-        </Form> <button type="submit">Search</button>
+        </Form> 
         <br />
         </div>
   )
