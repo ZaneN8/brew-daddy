ce05fe74b0fe8205445f11849a37a9c5b4ab1334
killNit <rileysanti@gmail.com>
2020-10-13 17:24:14 -0600

    styled the search bars

--- client/src/Components/SideBarSearch.js
+++ client/src/Components/SideBarSearch.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
+import styled from "styled-components";
 
 const SideBarSearch = ({
   setCityQuery,
@@ -11,36 +12,51 @@ const SideBarSearch = ({
 }) => {
   return (
     <div>
-      <Form>
-        <Form.Group>
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            autoFocus
-            placeholder="Enter to search a city here"
-            name="query"
-            value={cityQuery}
-            onChange={(e) => setCityQuery(e.target.value)}
-          />
-          <Form.Label>State</Form.Label>
-          <Form.Control
-            autoFocus
-            placeholder="Enter to search a State here"
-            name="query"
-            value={stateQuery}
-            onChange={(e) => setStateQuery(e.target.value)}
-          />
-          <Form.Label>Zip</Form.Label>
-          <Form.Control
-            autoFocus
-            placeholder="Enter to search a zip code here"
-            name="query"
-            value={zipQuery}
-            onChange={(e) => setZipQuery(e.target.value)}
-          />
-        </Form.Group>
-      </Form>
+      <StyledSmallForm>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              size="sm"
+              className="form"
+              autoFocus
+              placeholder="City"
+              name="query"
+              value={cityQuery}
+              onChange={(e) => setCityQuery(e.target.value)}
+            />
+
+            <Form.Control
+              size="sm"
+              className="form"
+              autoFocus
+              placeholder="State"
+              name="query"
+              value={stateQuery}
+              onChange={(e) => setStateQuery(e.target.value)}
+            />
+
+            <Form.Control
+              size="sm"
+              className="form"
+              autoFocus
+              placeholder="Zip Code"
+              name="query"
+              value={zipQuery}
+              onChange={(e) => setZipQuery(e.target.value)}
+            />
+          </Form.Group>
+        </Form>
+      </StyledSmallForm>
     </div>
   );
 };
+
+const StyledSmallForm = styled.div`
+  .form {
+    border-radius: 30px;
+    border: 1px solid;
+    color: black;
+  }
+`;
 
 export default SideBarSearch;
