90f94e7bde985c9ad7c61f619dab514e6e8b9c61
ZaneN8 <zanelnewsom@gmail.com>
2020-10-13 20:49:14 -0600

    forgot a save

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,2 +1,37 @@
 class Api::QuestionsController < ApplicationController
+  before_action :set_coffee_shop, only: [:index, :new, :create, :destroy]
+  def index 
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      render json: @question
+    else
+      redner json: @question.errors status: 422
+  end
+
+  def update
+    if @question.update(question_params)
+      render json: @question
+    else
+      render json: @question.errors status:422
+    end
+  end
+
+  def destroy
+    @question.destroy
+    render json: "Data deleted"
+  end
+
+  private
+
+  def set_coffee_shop
+    @coffee_shop = CoffeeShop.find(params[:coffee_shop_id])
+  end
 end
--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,4 +1,5 @@
 class Api::ReviewsController < ApplicationController
+  # before_action :authenticate_user!, only: [:create, :update, :destroy]
   before_action :set_coffee_shop, only: [:index, :new, :create, :destroy]
   before_action :set_review, only: [:update, :edit, :destroy]
   before_action :set_user, only: [:cu_reviews] 
