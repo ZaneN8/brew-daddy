0d2a80734aa19d34b2fa6e8be4e9f98587dbce28
killNit <rileysanti@gmail.com>
2020-10-15 16:25:26 -0600

    merged riley branches

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :review
   belongs_to :user
+
+  validates :user_id, uniqueness: { scope: :review_id,
+    message: "Brew Daddy says one vote only"}
 end
