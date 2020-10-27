f6be2d06eb3f6d348286e997feaa3982c61988c9
Jon Roberts <kewlguy@gmail.com>
2020-10-27 15:13:53 -0600

    Changed seed to show coffee shop pic instead of cat, and some style work

--- client/src/Components/search/CoffeeShopListItem.js
+++ client/src/Components/search/CoffeeShopListItem.js
@@ -8,6 +8,14 @@ import "react-rater/lib/react-rater.css";
 const CoffeeShopListItem = ({ coffee }) => {
   const [ratingsData, setRatingsData] = useState({});
 
+  const shopCost = () => {
+    const dollars = [];
+    for (let i = 0; i < coffee.cost; i++) {
+      dollars.push(<span>$</span>);
+    }
+    return dollars;
+  };
+
   useEffect(() => {
     axios
       .get(`/api/coffee_shops/${coffee.id}/average_stats`)
@@ -27,24 +35,22 @@ const CoffeeShopListItem = ({ coffee }) => {
           <img src={coffee.image} />
         </Column1>
         <Column2>
-          <Link as="h1" to={`/coffee_shops/${coffee.id}`}>
+          <Title><Link to={`/coffee_shops/${coffee.id}`}>
             {coffee.name}
-          </Link>
-          <br />
-          <b> Address: </b> {coffee.address} <br />
-          {coffee.city}, {coffee.state} <br />
-          <br />
-          <b> Phone Number: </b> {coffee.contact_info} <br />
-          {/* Rating: {ratingsData.total_rating} */}
-          <h1>
-            <Rater
+          </Link></Title>
+          <Rater
               total={5}
               interactive={false}
               rating={`${ratingsData.total_rating}`}
-            />
-          </h1>
+            /> {coffee.cost && shopCost()}
           <br />
-          {coffee.description}
+          <Contact>
+          <b> Address: </b> {coffee.address} {coffee.city}, {coffee.state} {coffee.zip}<br />
+          <b> Contact: </b> {coffee.contact_info} <br />
+         
+          {/* Rating: {ratingsData.total_rating} */}
+          <br />
+          {coffee.description} </Contact>
         </Column2>
       </Row>
     </StyledResultCard>
@@ -53,11 +59,12 @@ const CoffeeShopListItem = ({ coffee }) => {
 
 const StyledResultCard = styled.div`
   display: flex;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
   transition: 0.3s;
-  border: 1px solid;
+  border: 0px solid;
   border-radius: 30px;
-  padding: 1em;
+  padding: 0em;
+  margin: 2em;
   overflow: hidden;
   text-overflow: ellipsis;
 `;
@@ -69,11 +76,21 @@ const Row = styled.div`
 const Column1 = styled.div`
   flex: 3;
   display: flex;
-  flex-directionL column;
+  flex-direction: column;
 `;
 
 const Column2 = styled.div`
-  flex: 7;
+  // flex: 7;
+  padding: 1em;
 `;
+
+const Contact = styled.h5`
+  font-size: 12px;
+`;
+
+const Title = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+`
 
 export default CoffeeShopListItem;
